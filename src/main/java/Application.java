4dfaf4885ac2f9a46aa4e3e1c2fbbeb49d2f35e5
Connor Student <connorstudent@Thunder-Claus.local>
2019-03-18 18:59:01 -0700

    Added Help Menu

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,21 @@
 public class Application {
-    public static void main(final String[] args) {
-        System.out.println("Hello world!");
+    public static void main(final String[] arguments) {
+        final Application application = new Application();
+
+        if (arguments.length == 0) {
+            application.printHelpText();
+
+        }
+        for (final String argument : arguments) {
+            if (argument.equals("-h")) {
+                application.printHelpText();
+            }
+
+        }
+    }
+
+    private void printHelpText() {
+        System.out.println("help");
+        System.exit(0);
     }
 }
