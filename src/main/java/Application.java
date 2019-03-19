f01af1f9094ff4ade3e5b151049c7a73782913d9
Connor Student <connorstudent@Thunder-Claus.local>
2019-03-18 19:08:24 -0700

    git add .

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -16,7 +16,7 @@ public class Application {
     }
 
     private void printHelpText() {
-        System.out.println("Help!");
+        System.out.println("Help!!");
         System.exit(0);
     }
 }
